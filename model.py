993e66769c6e319226db21f26971cd8c5659b6cc
Alireza <kazemipour.alireza@gmail.com>
2021-08-04 16:50:54 +0430

    Implement worker.

---
+++ model.py
@@ -0,0 +1,59 @@
+from abc import ABC
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torch.distributions.normal import Normal
+
+
+class Actor(nn.Module, ABC):
+    def __init__(self, n_states, n_actions, action_bounds, n_hiddens=128):
+        super(Actor, self).__init__()
+        self.n_states = n_states
+        self.n_actions = n_actions
+        self.n_hiddens = n_hiddens
+        assert action_bounds[0] == -action_bounds[1], "computation of bounds of mu should change!"
+        self.action_bounds = action_bounds
+
+        self.hidden = nn.Linear(self.n_states, self.n_hiddens)
+        self.mu = nn.Linear(self.n_hiddens, self.n_actions)
+        self.sigma = nn.Linear(self.n_hiddens, self.n_actions)
+
+        nn.init.kaiming_normal_(self.hidden.weight, nonlinearity="relu")
+        self.hidden.bias.data.zero_()
+        nn.init.xavier_uniform_(self.mu.weight)
+        self.mu.bias.data.zero_()
+        nn.init.kaiming_normal_(self.sigma.weight, nonlinearity="softplus")
+        self.sigma.bias.data.zero_()
+
+    def forward(self, inputs):
+        x = inputs
+        x = F.relu(self.hidden(x))
+        mu = torch.tanh(self.mu(x))
+        sigma = F.softplus(self.sigma(x))
+
+        mu *= self.action_bounds[1]
+        mu.clamp_(self.action_bounds[0], self.action_bounds[1])
+
+        return Normal(mu, sigma)
+
+
+class Critic(nn.Module, ABC):
+    def __init__(self, n_states, n_hiddens=128):
+        super(Critic, self).__init__()
+        self.n_states = n_states
+        self.n_hiddens = n_hiddens
+
+        self.hidden = nn.Linear(self.n_states, self.n_hiddens)
+        self.value = nn.Linear(self.n_hiddens, 1)
+
+        nn.init.kaiming_normal_(self.hidden.weight, nonlinearity="relu")
+        self.hidden.bias.data.zero_()
+        nn.init.xavier_uniform_(self.value.weight)
+        self.value.bias.data.zero_()
+
+    def forward(self, inputs):
+        x = inputs
+        x = F.relu(self.hidden(x))
+        value = self.value(x)
+
+        return value
---
+++ worker.py
@@ -0,0 +1,106 @@
+from model import Actor, Critic
+import gym
+import numpy as np
+import torch
+from torch import from_numpy
+
+
+class Worker:
+    def __init__(self, n_states,
+                 n_actions,
+                 action_bounds,
+                 env_name,
+                 n_hiddens,
+                 global_actor,
+                 global_critic,
+                 global_actor_optimizer,
+                 global_critic_optimizer,
+                 gamma,
+                 ent_coeff):
+        self.n_states = n_states
+        self.n_actions = n_actions
+        self.action_bounds = action_bounds
+        self.n_hiddens = n_hiddens
+        self.gamma = gamma
+        self.ent_coeff = ent_coeff
+        self.env_name = env_name
+        self.env = gym.make(self.env_name)
+
+        self.local_actor = Actor(self.n_states, self.n_actions, self.action_bounds, self.n_hiddens)
+        self.local_critic = Critic(self.n_states, self.n_hiddens)
+
+        self.global_actor = global_actor
+        self.global_critic = global_critic
+        self.global_actor_optimizer = global_actor_optimizer
+        self.global_critic_optimizer = global_critic_optimizer
+
+    def get_action(self, state):
+        state = np.expand_dims(state, 0)
+        state = from_numpy(state).float()
+        with torch.no_grad():
+            dist = self.local_actor(state)
+            action = dist.sample()
+        return action.numpy()
+
+    def get_value(self, state):
+        state = np.expand_dims(state, 0)
+        state = from_numpy(state).float()
+        with torch.no_grad():
+            value = self.local_critic(state)
+        return value.numpy()
+
+    def sync_thread_spec_params(self):
+        self.local_actor.load_state_dict(self.global_actor.state_dict())
+        self.local_critic.load_state_dict(self.global_critic.state_dict())
+
+    @staticmethod
+    def share_grads_to_global_models(local_model, global_model):
+        for local_param, global_param in zip(local_model.parameters(), global_model.parameters()):
+            if global_param.grad is not None:
+                return
+            global_param._grad = local_param.grad
+
+    def step(self):
+        while True:
+            self.global_actor_optimizer.zero_grad()  # Reset global gradients
+            self.global_critic_optimizer.zero_grad()
+            self.sync_thread_spec_params()  # Synchronize thread-specific parameters
+
+            states, actions, rewards, dones, next_states = [], [], [], [], []
+            state = self.env.reset()
+            done = False
+            while not done:
+                action = self.get_action(state)
+                next_state, reward, done, _ = self.env.step(action)
+                states.append(state)
+                actions.append(action)
+                rewards.append(reward)
+                dones.append(dones)
+                next_states.append(next_state)
+
+            R = 0
+            for r in reversed(rewards):
+                R = r + self.gamma * R
+
+            states = torch.cat(states).view(-1, self.n_states)
+            actions = torch.cat(actions)
+            returns = torch.cat(R).view(-1, 1)
+
+            dist = self.local_actor(states)
+            log_probs = dist.log_prob(actions)
+
+            values = self.local_critic(states)
+            advs = returns - values
+
+            pg_loss = -(log_probs * advs.detach()).mean()
+            value_loss = (advs ** 2).mean()
+
+            actor_loss = pg_loss - self.ent_coeff * dist.entropy().mean()
+            actor_loss.backward()
+            value_loss.backward()
+
+            self.share_grads_to_global_models(self.local_actor, self.global_actor)
+            self.share_grads_to_global_models(self.local_critic, self.global_critic)
+
+            self.global_actor_optimizer.step()
+            self.global_critic_optimizer.step()
