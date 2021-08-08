ef39afaabe0d2ab0b0cf13ba4f78450a128a582a
Alireza <kazemipour.alireza@gmail.com>
2021-08-08 22:51:20 +0430

    Add base model.

--- model.py
+++ model.py
@@ -2,50 +2,65 @@ from abc import ABC
 import torch
 from torch import nn
 from torch.nn import functional as F
-from torch.distributions.categorical import Categorical
+from torch.distributions.multivariate_normal import MultivariateNormal
 
 
 class Actor(nn.Module, ABC):
-    def __init__(self, n_states, n_actions, n_hiddens=128):
+    def __init__(self, n_states, n_actions, action_bounds, n_hiddens=128):
         super(Actor, self).__init__()
         self.n_states = n_states
         self.n_actions = n_actions
         self.n_hiddens = n_hiddens
+        assert action_bounds[0] == -action_bounds[1], "computation of bounds of the mu should change!"
+        self.action_bounds = action_bounds
 
         self.hidden = nn.Linear(self.n_states, self.n_hiddens)
-        self.logits = nn.Linear(self.n_hiddens, self.n_actions)
+        self.mu = nn.Linear(self.n_hiddens, self.n_actions)
 
         nn.init.kaiming_normal_(self.hidden.weight, nonlinearity="relu")
         self.hidden.bias.data.zero_()
-        nn.init.xavier_uniform_(self.logits.weight)
-        self.logits.bias.data.zero_()
+        nn.init.xavier_uniform_(self.mu.weight)
+        self.mu.bias.data.zero_()
 
     def forward(self, inputs):
         x = inputs
         x = F.relu(self.hidden(x))
-        probs = F.softmax(self.logits(x), dim=-1)
+        mu = self.mu(x)
+        # mu = mu * self.action_bounds[1]
 
-        return Categorical(probs), probs
+        return MultivariateNormal(mu, 0.3 * torch.eye(mu.size()))
 
 
-class Critic(nn.Module, ABC):
+class SDNCritic(nn.Module, ABC):
     def __init__(self, n_states, n_actions, n_hiddens=128):
-        super(Critic, self).__init__()
+        super(SDNCritic, self).__init__()
         self.n_states = n_states
         self.n_actions = n_actions
         self.n_hiddens = n_hiddens
 
-        self.hidden = nn.Linear(self.n_states, self.n_hiddens)
-        self.q_value = nn.Linear(self.n_hiddens, self.n_actions)
+        self.hidden_value = nn.Linear(self.n_states, self.n_hiddens)
+        self.value = nn.Linear(self.n_hiddens, 1)
+        self.hidden_adv = nn.Linear(self.n_states + self.n_actions, self.n_hiddens)
+        self.adv = nn.Linear(self.n_hiddens, 1)
 
-        nn.init.kaiming_normal_(self.hidden.weight, nonlinearity="relu")
-        self.hidden.bias.data.zero_()
-        nn.init.xavier_uniform_(self.q_value.weight)
-        self.q_value.bias.data.zero_()
+        nn.init.kaiming_normal_(self.hidden_value.weight, nonlinearity="relu")
+        self.hidden_value.bias.data.zero_()
+        nn.init.kaiming_normal_(self.hidden_adv.weight, nonlinearity="relu")
+        self.hidden_adv.bias.data.zero_()
+        nn.init.xavier_uniform_(self.value.weight)
+        self.value.bias.data.zero_()
+        nn.init.xavier_uniform_(self.adv.weight)
+        self.adv.bias.data.zero_()
 
-    def forward(self, inputs):
+    def forward(self, inputs, a, u):
         x = inputs
-        x = F.relu(self.hidden(x))
-        q_value = self.q_value(x)
+        x = F.relu(self.hidden_value(x))
+        value = self.value(x)
 
-        return q_value
+        x = F.relu(self.hidden_adv(torch.cat([inputs, a], dim=-1)))
+        adv = self.adv(x)
+
+        x = F.relu(self.hidden_adv(torch.cat([inputs, u], dim=-1)))
+        adv_mean = self.adv(x).mean(1, keepdim=True)
+
+        return value + adv - adv_mean
